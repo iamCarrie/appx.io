135d19d9379eeeced88e4612e94fa8a98dbcb8d6
carrielin <carrielin@sugarfun.com.tw>
2023-08-12 21:49:19 +0800

    update

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,6 +1,27 @@
 import 'index.css';
+import { createApp } from 'petite-vue';
 
 const requireAll = (requireContext) => requireContext.keys().map(requireContext);
 const req = require.context('_svg/', false, /\.svg$/);
 
 requireAll(req);
+
+// window.onload = function () {
+//   const menuButton = document.querySelector('.j-menu-btn');
+
+//   menuButton.onclick = function () {
+//     const menuContent = document.querySelector('.j-menu-content');
+
+//     menuContent.classList.toggle('menu-active');
+//   };
+// };
+
+createApp({
+  // exposed to all expressions
+  count: 0,
+  // getters
+  get plusOne() {
+    return this.count + 1;
+  }
+
+}).mount('#app');
